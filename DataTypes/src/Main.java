937787f77b14d29653e49af54e80d1851cb6517f
nsavas2000 <83330167+nsavas2000@users.noreply.github.com>
2022-09-17 20:07:15 +0300

    Secand Commit
    Data types

---
+++ DataTypes/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int num = 12;
+        num = 13;
+        char character = 'A';
+        System.out.println(num);
+        System.out.println(character);
+    }
+}
